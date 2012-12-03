74384e59e17316713a723ed9d8dd91054f83b0cb
Raymond Wainman <raywainman@gmail.com>
2012-12-03 15:05:14 -0700

    increasing server capacity

--- server/server.rb
+++ server/server.rb
@@ -4,7 +4,7 @@ require_relative "./server_manager"
 
 class Server
   def initialize(port)
-    @server = XMLRPC::Server.new(port, ENV["HOSTNAME"])
+    @server = XMLRPC::Server.new(port, ENV["HOSTNAME"], 100)
     @server.add_handler("server", ServerManager.new())
     @server.serve()
   end
